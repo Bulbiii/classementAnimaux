fedfcae7a897a1448fae1f84e2f97529f714cba0
jtrognon <julietrognon74@gmail.com>
2025-04-14 12:41:05 +0200

    test

--- test.php
+++ test.php
@@ -13,6 +13,8 @@ $res2 = selectPhoto($con, $id_test);
 $res3 = selectSolutions($con, $id_test);
 $res4 = selectDescr($con, $id_test);
 
+echo "toto";
+
 echo "<pre>";
 print_r($resultat); 
 echo "</pre>";
